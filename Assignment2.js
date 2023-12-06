bcc23c5de0b983f96c44ee52fb1db11ba1055e8c
Youssef Ali <41948774+Youssef-joe@users.noreply.github.com>
2023-12-06 14:20:35 +0200

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,114 @@
+let box = document.querySelector('.color1')
+let colors = ['red', 'navy', 'green', 'white', 'black', 'yellow']
+
+box.addEventListener('mouseenter', function(){
+    box.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box2 = document.querySelector('.color2')
+box2.addEventListener('mouseenter', function(){
+    box2.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box3 = document.querySelector('.color3')
+box3.addEventListener('mouseenter', function(){
+    box3.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box4 = document.querySelector('.color4')
+box4.addEventListener('mouseenter', function(){
+    box4.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box5 = document.querySelector('.color5')
+box5.addEventListener('mouseenter', function(){
+    box5.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box6 = document.querySelector('.colortwo1')
+box6.addEventListener('mouseenter', function(){
+    box6.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box7 = document.querySelector('.colortwo2')
+box7.addEventListener('mouseenter', function(){
+    box7.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box8 = document.querySelector('.colortwo3')
+box8.addEventListener('mouseenter', function(){
+    box8.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box9 = document.querySelector('.colortwo4')
+box9.addEventListener('mouseenter', function(){
+    box9.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box10 = document.querySelector('.colortwo5')
+box10.addEventListener('mouseenter', function(){
+    box10.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box11 = document.querySelector('.colorthree1')
+box11.addEventListener('mouseenter', function(){
+    box11.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+let box12 = document.querySelector('.colorthree2')
+box12.addEventListener('mouseenter', function(){
+    box12.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box13 = document.querySelector('.colorthree3')
+box13.addEventListener('mouseenter', function(){
+    box13.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box14 = document.querySelector('.colorthree4')
+box14.addEventListener('mouseenter', function(){
+    box14.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box15 = document.querySelector('.colorthree5')
+box15.addEventListener('mouseenter', function(){
+    box15.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box16 = document.querySelector('.colorfour1')
+box16.addEventListener('mouseenter', function(){
+    box16.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box17 = document.querySelector('.colorfour2')
+box17.addEventListener('mouseenter', function(){
+    box17.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box18 = document.querySelector('.colorfour3')
+box18.addEventListener('mouseenter', function(){
+    box18.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box19 = document.querySelector('.colorfour4')
+box19.addEventListener('mouseenter', function(){
+    box19.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box20 = document.querySelector('.colorfour5')
+box20.addEventListener('mouseenter', function(){
+    box20.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box21 = document.querySelector('.colorfive1')
+box21.addEventListener('mouseenter', function(){
+    box21.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box22 = document.querySelector('.colorfive2')
+box22.addEventListener('mouseenter', function(){
+    box22.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box23 = document.querySelector('.colorfive3')
+box23.addEventListener('mouseenter', function(){
+    box23.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box24 = document.querySelector('.colorfive4')
+box24.addEventListener('mouseenter', function(){
+    box24.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+let box25 = document.querySelector('.colorfive5')
+box25.addEventListener('mouseenter', function(){
+    box25.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+})
+
+
